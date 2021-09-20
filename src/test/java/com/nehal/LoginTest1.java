5c6dae591a8fd43559006af1715f5b0e372d3925
nehalejazi <mnejazi@gmail.com>
2021-09-21 00:09:46 +0100

    Second

--- src/test/java/com/nehal/LoginTest1.java
+++ src/test/java/com/nehal/LoginTest1.java
@@ -3,14 +3,16 @@ package com.nehal;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class LoginTest1 {
 	
+	@Parameters("Browser")
 	@Test
-	public void login1()
+	public void login1(String browserlocal)
 	{
 		WebDriverManager.chromedriver().setup();
 		WebDriver driver = new ChromeDriver();
@@ -19,6 +21,7 @@ public class LoginTest1 {
 		driver.get("https://opensource-demo.orangehrmlive.com/");
 		String title = driver.getTitle();
 		System.out.println(title);
+		System.out.println("Paramere testing: "+browserlocal);
 		driver.findElement(By.xpath("//input[@id='txtUsername']")).sendKeys("Admin");
 		driver.findElement(By.xpath("//input[@id='txtPassword']")).sendKeys("admin123");
 		driver.findElement(By.xpath("//input[@id='btnLogin']")).click();
